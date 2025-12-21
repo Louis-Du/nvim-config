b64cef9122f4edb0c4921cda8268b3107d316de6
Louis-Du <luis.a.d.f.beto@gmail.com>
2025-12-21 13:02:52 -0500

    feature: se implementa una función para facilitar la estructura del códigi

--- snippets/java.lua
+++ snippets/java.lua
@@ -1,47 +1,28 @@
 local ls = require("luasnip")
 local s = ls.snippet
-local t = ls.text_node
 local i = ls.insert_node
+local t = ls.text_node
 
-return {
+-- Clase básica
+ls.add_snippets("java", {
+  s("class", {
+    t("public class "),
+    i(1, "NombreClase"),
+    t({" {", "", "    "}),
+    i(2),
+    t({"", "}"})
+  }),
+
+-- main
   s("main", {
     t({
       "public static void main(String[] args) {",
       "    "
     }),
-    i(1, "System.out.println(\"Hello World\");"),
+    i(1),
     t({
       "",
       "}"
     })
   }),
-
-  s("sysout", {
-    t("System.out.println("),
-    i(1, "\"\""),
-    t(");")
-  }),
-
-  s("fori", {
-    t("for (int "),
-    i(1, "i"),
-    t(" = 0; "),
-    i(2, "i"),
-    t(" < "),
-    i(3, "n"),
-    t("; "),
-    i(4, "i"),
-    t("++) {"),
-    t({ "", "    " }),
-    i(5),
-    t({ "", "}" })
-  }),
-
-  s("class", {
-    t("public class "),
-    i(1, "MyClass"),
-    t({ " {", "", "    " }),
-    i(2),
-    t({ "", "}" })
-  }),
-}
+})
